77c9af4028819fe23ef9bbebeead49c8b06d39ed
assaf <445552+asssaf@users.noreply.github.com>
2026-06-19 12:00:57 -0700

    refactor: implement feature-first project structure (section 1)

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:hello_world/main.dart';
+import 'package:hello_world/src/app.dart';
 
 void main() {
   testWidgets('Hello World UI verification test', (WidgetTester tester) async {
